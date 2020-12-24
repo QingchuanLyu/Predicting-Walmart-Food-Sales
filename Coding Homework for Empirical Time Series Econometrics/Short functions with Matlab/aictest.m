1380f8805e62e239ce1934f5872f1927cd038d76
QingchuanLyu <39771080+QingchuanLyu@users.noreply.github.com>
2020-12-24 00:25:00 -0800

    Delete aictest.m

--- Coding Homework for Empirical Time Series Econometrics/Short functions with Matlab/aictest.m
+++
@@ -1,7 +0,0 @@
-p=(1:8);
-estMdlResults = cell(numel(p),1); % Preallocation
-for j = 1:numel(p)
-    Mdl = varm(3,p(j));
-    EstMdl = estimate(Mdl,y);
-    estMdlResults{j} = summarize(EstMdl);
-end
